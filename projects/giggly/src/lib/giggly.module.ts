14ebba8512fac407386c1a756c1820357788d282
vivekks1293 <vivekks1293@gmail.com>
2025-01-18 14:22:50 +0530

    icon button feature

--- projects/giggly/src/lib/giggly.module.ts
+++ projects/giggly/src/lib/giggly.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 import { GigglyButtonComponent } from '../lib/components/buttons/giggly-button/giggly-button.component';
+import { IconButtonComponent } from '../lib/components/buttons/icon-button/icon-button.component';
 
 @NgModule({
-  declarations: [GigglyButtonComponent],
+  declarations: [GigglyButtonComponent, IconButtonComponent],
   imports: [CommonModule], // Import CommonModule here
-  exports: [GigglyButtonComponent],
+  exports: [GigglyButtonComponent, IconButtonComponent],
 })
 export class GigglyModule {}
--- projects/giggly/src/public-api.ts
+++ projects/giggly/src/public-api.ts
@@ -1,2 +1,3 @@
 export * from './lib/giggly.module';
 export * from './lib/components/buttons/giggly-button/giggly-button.component';
+export * from './lib/components/buttons/icon-button/icon-button.component';
